688793f2ca3a9854591d69d8eccff37628dc30c9
tony-gueville <gueville.tony@orange.fr>
2023-06-21 18:03:18 +0200

    Ajoute des cards dynamique

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="logo">
-      <img src="" alt="logo recact" />
+      <img src="./logo192.png" alt="logo recact" />
       <h3>React World</h3>
     </div>
   );
