0aab6ae8be8da5f2c24e9a240800cfb48387a0a7
tony-gueville <gueville.tony@orange.fr>
2023-06-21 16:59:14 +0200

    components navigation

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A oriois</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+        earum, cupiditate est quisquam quas consequatur illo vitae? Ratione
+        possimus id at blanditiis rem qui ea illum sunt, quidem sed? Molestias!
+      </p>
+    </div>
+  );
+};
+
+export default About;
